ef5c45f3ee7c2aee6187a9f4a2a87855d82697a1
glushik <38889735+glushik@users.noreply.github.com>
2018-05-27 23:48:14 +0300

    Add files via upload

--- start.js
+++ start.js
@@ -1,3 +1,18 @@
 function scroll_to_top(){
     $("html,body").animate({"scrollTop":0}, 650);
 }
+function showVideo(){
+    $.ajax({
+        url: "./nino.mp4",
+        crossDomain: true,
+        contentType: "video/mp4",
+        success: function(data){
+
+            
+             console.log(data);
+        }
+    })
+}
+function show(){
+    $(".video").append("video")
+}
